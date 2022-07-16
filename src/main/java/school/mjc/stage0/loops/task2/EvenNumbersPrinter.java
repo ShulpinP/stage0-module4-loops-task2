38ff17030348dbbb88eaf284589044ca6aa193b1
ShulpinP <pavelshulpin6@gmail.com>
2022-07-16 16:55:53 +0300

    1

--- src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
@@ -2,12 +2,13 @@ package school.mjc.stage0.loops.task2;
 
 public class EvenNumbersPrinter {
     public void printEvenNumbers(int printTillInclusive) {
-        int i = 0;
         if (printTillInclusive < 0) {
+            int i =-2;
             while (i >= printTillInclusive) {
                 System.out.println(i);
                 i = i - 2;}
             } else if (printTillInclusive > 0){
+                int i = -2;
                 while (i <= printTillInclusive) {
                     System.out.println(i);
                     i = i + 2;
--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
-        int i = 0;
+        int i = 1;
         while (i<=chars.length) {
             System.out.print(chars[i]);
             i++;
