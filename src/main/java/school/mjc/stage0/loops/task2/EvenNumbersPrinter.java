1ac286bae58a311bec119a4ad2306c21771b06f0
ShulpinP <pavelshulpin6@gmail.com>
2022-07-16 16:57:36 +0300

    1

--- src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
@@ -8,7 +8,7 @@ public class EvenNumbersPrinter {
                 System.out.println(i);
                 i = i - 2;}
             } else if (printTillInclusive > 0){
-                int i = -2;
+                int i = 2;
                 while (i <= printTillInclusive) {
                     System.out.println(i);
                     i = i + 2;
